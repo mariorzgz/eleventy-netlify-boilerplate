4bc4d1633eff4a440bebd4b0ebb2410ccc6e8451
mariorzgz <mario.rod.gonz97@gmail.com>
2022-12-04 01:34:06 +0100

    Updated netlify cms

--- _includes/assets/js/inline.js
+++
@@ -1,6 +0,0 @@
-// Add your inline JS here
-import CMS from 'netlify-cms-app'
-// Initialize the CMS object
-CMS.init()
-// Now the registry is available via the CMS object.
-CMS.registerPreviewTemplate('my-template', MyTemplate)
